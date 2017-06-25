974f20a7e614f86e15a0e4bc9cd097b1976ed228
Charlie Duong <charlieduong94@gmail.com>
2017-06-24 21:41:22 -0400

    [NO-ISSUE] Re add fashion model defaults to config

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,7 +1,10 @@
 // config model
 const BaseConfig = require('windbreaker-service-util/models/BaseServiceConfig')
+const DefaultsMixin = require('fashion-model-defaults')
 
 module.exports = BaseConfig.extend({
+  mixins: [ DefaultsMixin ],
+
   properties: {
     amqUrl: {
       description: 'The url used to access activeMQ',
