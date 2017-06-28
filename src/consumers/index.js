be0c477ee2722fe739362c056c35558026ec9cf7
Charlie Duong <charlieduong94@gmail.com>
2017-06-27 20:00:17 -0400

    Wb 32 managed consumers (#8)
    * [WB-32] Switch to managed consumers
    
    * [WB-32] minor comments fix
    
    * [WB-32] renamed _restartConsumer to _restart
    
    * [WB-32] Use queue util's createManagedConsumer

--- src/consumers/index.js
+++ src/consumers/index.js
@@ -1,6 +1,6 @@
 const config = require('~/src/config')
 const logger = require('~/src/logging').logger(module)
-const queue = require('windbreaker-service-util/queue')
+const queueUtil = require('windbreaker-service-util/queue')
 
 const EVENTS_QUEUE_NAME = config.getEventsQueueName()
 const EVENTS_QUEUE_PREFETCH_COUNT = config.getEventsQueuePrefetchCount()
@@ -11,10 +11,11 @@ const CONSUMER_RECONNECT_TIMEOUT = config.getConsumerReconnectTimeout()
 exports.initialize = async function () {
   const amqUrl = config.getAmqUrl()
 
-  // Create events queue consumer
-  await queue.createConsumer({
+  // returns a managed consumer instance
+  await queueUtil.createManagedConsumer({
     amqUrl,
     logger,
+    restart: true,
     reconnectTimeout: CONSUMER_RECONNECT_TIMEOUT,
     consumerOptions: {
       queueName: EVENTS_QUEUE_NAME,
@@ -26,10 +27,10 @@ exports.initialize = async function () {
     }
   })
 
-  // Create work queue consumer
-  await queue.createConsumer({
+  await queueUtil.createManagedConsumer({
     amqUrl,
     logger,
+    restart: true,
     reconnectTimeout: CONSUMER_RECONNECT_TIMEOUT,
     consumerOptions: {
       queueName: WORK_QUEUE_NAME,
@@ -40,4 +41,15 @@ exports.initialize = async function () {
       await Promise.resolve()
     }
   })
+
+  // In the future, we may want fine control over the consumer instances
+  // managedConsumer.on('consumer-restarted', (simpleConsumer) => {
+  //  // handle new consumer
+  // })
+  //
+  // we may also handle situations where we fail to create consumers
+  // (possibly kill process and allow server to reset)
+  // managedConsumer.on('consumer-restart-failed', (err) => {
+  //  // handle restart failure
+  // })
 }
