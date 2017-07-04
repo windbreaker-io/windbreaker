71b96054d145bb6c447e222e3de91db629abe982
James Beavers <jamesjbeavers@gmail.com>
2017-07-04 10:53:31 -0400

    [NO-ISSUE] Use containers for both CI job rather than host services (#11)
    * [NO-ISSUE] Use same test path on CI (no services running on host)
    
    * [NO-ISSUE] Use dynamic ports for docker containers
    
    * [NO-ISSUE] No need to export main pid as an env variable

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,6 @@ const registerConfig = require('windbreaker-service-util/config')
 const Config = require('./Config')
 const config = module.exports = new Config()
 
-registerConfig(config)
+registerConfig(config, [
+  { amqUrl: `amqp://localhost:${process.env.AMQ_PORT || 5672}` }
+])
