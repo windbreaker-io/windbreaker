a081b75245aab445272bb7bf6c3e3b380035cf6d
Druotic <jamesjbeavers@gmail.com>
2017-10-13 14:45:28 -0400

    [WB-25] Simply exports

--- src/dao/knexfile.js
+++ src/dao/knexfile.js
@@ -4,7 +4,4 @@ const config = require('~/src/config')
 config.load()
 
 const environment = config.getEnvironment().name().toLowerCase()
-let exported = {}
-exported[environment] = config.getKnex().clean()
-
-module.exports = exported
+exports[environment] = config.getKnex().clean()
